fac1b5cb3c6c7b7a562f471ebe39ad790e89f964
Abhay Rajput <abhayrajpoot389@gmail.com>
2022-02-16 15:31:07 +0530

    updated

---
+++ sqltable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Employeeinfo(
+    EmpId INT PRIMARY KEY,
+    EmpFname VARCHAR(20),
+    EmpLname VARCHAR(20),
+    Department VARCHAR(20),
+    Salary INT
+);
--- test3.cpp
+++ test3.cpp
@@ -1,42 +1,11 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-typedef long long ll;
-const int sz = 2e5 + 5, mod = 1e9 + 7;
-
-ll fac[sz];
-
-void solve()
-{
-    int n;
-    cin >> n;
-    map<int, int> mp;
-    for (int i = 0, x; i < n; i++)
-        scanf("%d", &x), mp[x]++;
-
-    map<int, int> d;
-    for (auto &it : mp)
-    {
-        int c = it.second;
-        for (int i = 1; i <= c; i++)
-        {
-            int y = c - i, x = i - 1;
-            d[y - x]++;
-        }
-    }
-
-    ll ans = 1;
-    for (auto &it : d)
-        ans = ans * fac[it.second] % mod;
-    cout << ans << endl;
-}
+int mem[100];
+int dp[100][100];
 
 int main()
 {
-    fac[0] = 1;
-    for (int i = 1; i < sz; i++)
-        fac[i] = fac[i - 1] * i % mod;
-    int t;
-    cin >> t;
-    while (t--)
-        solve();
+    
+    return 0;
 }
