6ebafa3272221e66318e0a545ad77527a5981d52
Abhay Rajput <abhayrajpoot389@gmail.com>
2022-03-11 00:12:22 +0530

    updated

--- test3.cpp
+++ test3.cpp
@@ -1,11 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 using namespace std;
 int mem[100];
 int dp[100][100];
 
+void convert(int num)
+{
+    char arr[100];
+    int i = 0;
+    while (num != 0)
+    {
+        int temp = 0;
+        temp = num % 16;
+        if (temp < 10)
+        {
+            arr[i] = temp + 48;
+            i++;
+        }
+        else
+        {
+            arr[i] = temp + 55;
+            i++;
+        }
+        num = num / 16;
+    }
+    cout << "0x";
+    for (int j = i - 1; j >= 0; j--)
+        cout << arr[j];
+}
+
 int main()
 {
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        float b;
+        float c;
+        cin >>
+            a;
+        convert(a);
+        cout << "/n";
+        cin >> b;
+        
+    }
+
     return 0;
 }
